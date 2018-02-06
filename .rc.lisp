b7f8320e11748301272f323ee2cc3c361ee5439d
Sebastian Christ <rudolfo.christ@gmail.com>
2018-02-06 15:13:50 +0100

    Enable ACL REPL in SBCL

--- .rc.lisp
+++ .rc.lisp
@@ -1,4 +1,4 @@
-;;; common init file for various CLs
+;;; common init file for various CLs  -*- mode: lisp; -*-
 ;;; see also http://www.cliki.net/Common%20Lisp%20implementation
 ;;; for startup file names
 ;;; - ~./clisprc.lisp (clisp)
@@ -25,12 +25,65 @@
   (require :abcl-asdf)
   (require :jss))
 
-;;; regex
-#+quicklisp (ql:quickload :cl-ppcre)
+;;; libs
+#+quicklisp (ql:quickload '(:clutils) :silent t)
 
-;;; unit testing
-#+quicklisp (ql:quickload :fiveam)
+;;; Allegro CL REPL
+#+sbcl
+(progn
+  (ignore-errors (require 'sb-aclrepl))
+  (when (find-package :sb-aclrepl)
+    (push :aclrepl *features*)))
 
-;;; personal utilities
-#+ (and asdf3.1 quicklisp (not abcl))
-(ql:quickload :clutils)
+#+aclrepl
+(defun ovwrt-exit-cmd (&optional (status 0))
+  "Exit command overwrite that doesn't terminate threads."
+  #+sb-thread
+  (flet ((other-threads ()
+           (delete sb-thread:*current-thread*
+                   (sb-thread:list-all-threads))))
+    (let ((other-threads (other-threads)))
+      (when other-threads
+        (format *standard-output* "There exists the following processes~%")
+        (format *standard-output* "~{~A~%~}" other-threads)
+        (format *standard-output* "Do you want to exit lisp anyway [n]? ")
+        (force-output *standard-output*)
+        (let ((input (string-trim '(#\Space #\Tab #\Return)
+                                  (read-line *standard-input*))))
+          (unless (and (plusp (length input))
+                       (or (char= #\y (char input 0))
+                           (char= #\Y (char input 0))))
+            (return-from ovwrt-exit-cmd))))))
+  (sb-ext:exit :code status))
+
+#+aclrepl
+;;; This is super hackish.
+(let ((cmd (sb-aclrepl::find-cmd "exit")))
+  (setf (sb-aclrepl::cmd-table-entry-func cmd) #'ovwrt-exit-cmd)
+  (sb-aclrepl::%add-entry
+   cmd
+   (sb-aclrepl::cmd-table-entry-abbr-len cmd)))
+
+#+(and aclrepl quicklisp)
+(sb-aclrepl:alias ("ql" 1 "quickload system") (system)
+                  (ql:quickload system))
+
+#+(and aclrepl asdf)
+(sb-aclrepl:alias ("make" 1 "asdf:make system") (system)
+                  (asdf:make system))
+
+;;; machine information
+(defun machine-info ()
+  "Print this host's details."
+  (format t "~@{~A: ~A~^~%~}~%"
+          "Machine Information"  ""
+          "-- Arch"  (machine-type)
+          "-- CPU"  (machine-version)
+          "-- OS" (software-type)
+          "-- Version" (software-version)
+          "-- Hostname"  (machine-instance)
+          "-- HOME"  (user-homedir-pathname)
+          "-- Site Name"  (list (short-site-name) (long-site-name))
+          "Lisp Information"  ""
+          "-- Implementation"  (lisp-implementation-type)
+          "-- Version"  (lisp-implementation-version)))
