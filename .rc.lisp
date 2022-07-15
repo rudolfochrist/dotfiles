3514a561c4cb50bca7fb2b857b20eee3abe57b48
Sebastian Christ <rudolfo.christ@gmail.com>
2022-07-15 10:47:28 +0200

    Adjust QL setup to use HTTPS

--- .rc.lisp
+++ .rc.lisp
@@ -16,16 +16,40 @@
 (proclaim '(optimize (speed 0) (compilation-speed 0) (safety 3) (debug 3)))
 
 ;;; quicklisp
+;;;
+;;; QL Installation:
+;;;
+;;; 1. Clone quicklisp-client from https://github.com/quicklisp/quicklisp-client.git
+;;;    to ~/quicklisp
+;;;
+;;; 2. Clone ql-https from https://github.com/rudolfochrist/ql-https.git to
+;;;    to ~/common-lisp/ql-https
+;;;
+;;; 3. In ~/quicklisp/setup.lisp comment out the last line (quicklisp:setup)
+;;;
+;;; 4. Start a fresh REPL and (require 'asdf)
+;;;
+;;; 5. Load ~/quicklisp/setup.lisp
+;;;
+;;; 6. Eval (asdf:load-system "ql-https")
+;;;
+;;; 7. Eval (quicklisp:setup) - use the USE-HTTPS restart if you hit the network
+;;;
+;;; 8. DONE
 (let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
                                        (user-homedir-pathname))))
   (when (probe-file quicklisp-init)
-    (load quicklisp-init)))
-
-#+quicklisp
-(asdf:load-system "ql-https" :verbose nil)
+    (load quicklisp-init)
+    (asdf:load-system "ql-https")
+    (uiop:symbol-call :quicklisp :setup)))
 
 #+ql-https
 (setf ql-https:*quietly-use-https* t)
+
+#+quicklisp
+(progn
+  (ql:quickload "ql-file-fetcher" :verbose nil :silent t)
+  (ql:quickload "project-loader" :verbose nil :silent t))
 
 #+sbcl
 (setf sb-impl::*default-external-format* :utf-8)
